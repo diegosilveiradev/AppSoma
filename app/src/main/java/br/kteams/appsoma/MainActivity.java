ec89449105c4fe99de254f38d51de6cad1b10f05
diegosilveiradev <diegosilveira.dev@gmail.com>
2022-01-17 14:16:49 -0300

    subir projeto

---
+++ app/src/main/java/br/kteams/appsoma/MainActivity.java
@@ -0,0 +1,17 @@
+package br.kteams.appsoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  res;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
